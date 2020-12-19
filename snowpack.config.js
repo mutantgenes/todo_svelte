69384dfe8bb76580b6f4a252b601f852c6fd3261
Richard Walker <richard.walker@ekmglobal.com>
2020-12-19 00:23:43 +0000

    basic Svelte TODO app

---
+++ snowpack.config.js
@@ -0,0 +1,28 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/dist',
+  },
+  plugins: [
+    '@snowpack/plugin-svelte'
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
